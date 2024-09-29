9c0312acd9eaccce5d5d378e3de3c1975b42ef04
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2024-09-29 22:08:46 +0200

    commit

--- src/content/resume.js
+++ src/content/resume.js
@@ -1,33 +1,33 @@
 export const data = [
   {
-    category: "Education",
-    items: [
-      {
-        school: "Rødovre Gymnasium",
-        program: "STX Music A and English A",
-        year: "Jun 2016",
-      },
-      {
-        school: "KEA - Copenhagen School of Design and Technology",
-        program: "Multimedia Design",
-        year: "Aug 2023 - current",
-      },
-    ],
-  },
-  {
     category: "Experience",
     items: [
+      {
+        company: "KEA - Copenhagen School of Design and Technology",
+        role: "Student Assistant Tutor",
+        year: "Aug 2024 - current",
+        desc: "<p>Part time tutoring of multimedia design and frontend development students, aiding students in problem-solving code for assignments.</p>",
+      },
       {
         company: "Concentrix",
         role: "E-commerce CS Representative",
         year: "Jul 2018 - Jul 2020",
         desc: "<p>Danish E-commerce Customer Support Agent for a large online payment service. Backup support for the Swedish and Dutch customer service teams.</p>",
       },
+    ],
+  },
+  {
+    category: "Education",
+    items: [
       {
-        company: "KEA - Copenhagen School of Design and Technology",
-        role: "Student Assistant Tutor",
-        year: "Aug 2024 - current",
-        desc: "<p>Part time tutoring of multimedia design and frontend development students, aiding students in problem-solving code for assignments.</p>",
+        school: "KEA - Copenhagen School of Design and Technology",
+        program: "Multimedia Design",
+        year: "Aug 2023 - current",
+      },
+      {
+        school: "Rødovre Gymnasium",
+        program: "STX Music A and English A",
+        year: "Jun 2016",
       },
     ],
   },
